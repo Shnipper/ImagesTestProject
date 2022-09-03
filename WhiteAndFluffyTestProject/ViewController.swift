5c28d5dbb36e983c177e5f8e49f16c1fba0356f0
Voloshenko Evgeniu <95745884+Shnipper@users.noreply.github.com>
2022-09-03 18:17:09 +0300

    Initial Commit

---
+++ WhiteAndFluffyTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteAndFluffyTestProject
+//
+//  Created by Евгений Волошенко on 03.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
